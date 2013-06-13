94308d2e079d580bb0737ee6e088bbcef1b8c403
Alex Robbin <alex.robbin@meyouhealth.com>
2013-06-13 15:29:26 -0400

    use the previous incarnation of #input_class for FormBuilder
    For some reason, Formtastic checks if a constant is defined first, which doesn't play nicely with autoloading of custom inputs. This brings Formtastic's implementation back to pre justinfrench/formtastic#783

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -130,24 +130,9 @@ module ActiveAdmin
       "ActiveAdmin::Inputs::#{as.to_s.camelize}Input"
     end
 
-    # prevent exceptions in production environment for better performance
-    def input_class_with_const_defined(as)
-      input_class_name = custom_input_class_name(as)
-
-      if ::Object.const_defined?(input_class_name)
-        input_class_name.constantize
-      elsif ActiveAdmin::Inputs.const_defined?(input_class_name)
-        active_admin_input_class_name(as).constantize
-      elsif Formtastic::Inputs.const_defined?(input_class_name)
-        standard_input_class_name(as).constantize
-      else
-        raise Formtastic::UnknownInputError
-      end
-    end
-
-    # use auto-loading in development environment
-    def input_class_by_trying(as)
-      begin
+    def input_class(as)
+      @input_classes_cache ||= {}
+      @input_classes_cache[as] ||= begin
         begin
           custom_input_class_name(as).constantize
         rescue NameError
@@ -157,9 +142,9 @@ module ActiveAdmin
             standard_input_class_name(as).constantize
           end
         end
+      rescue NameError
+        raise Formtastic::UnknownInputError
       end
-    rescue NameError
-      raise Formtastic::UnknownInputError
     end
 
     # This method calls the block it's passed (in our case, the `f.inputs` block)
